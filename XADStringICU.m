ce324222a2e39568b97991a59fa6b53af4acb219
Dag Ågren <paracelsus@gmail.com>
2010-07-05 01:48:40 +0300

    Trying ICU instead since iconv is horrible

---
+++ XADStringICU.m
@@ -0,0 +1,81 @@
+#import "XADString.h"
+
+#import <unicode/ucnv.h>
+
+@implementation XADString (PlatformSpecific)
+
++(NSString *)stringForData:(NSData *)data encodingName:(NSString *)encoding
+{
+	if([data length]==0) return @"";
+
+	UErrorCode err;
+	UConverter *conv=ucnv_open(,&err);
+	if(!conv) return nil;
+
+	int numbytes=[data length];
+	const char *bytebuf=[data bytes];
+
+	ucnv_setToUCallBack(conv,UCNV_TO_U_CALLBACK_STOP,NULL,NULL,NULL,&err);
+	if(U_FAILURE(err)) { ucnv_close(conv); return nil; }
+
+	int numchars=ucnv_toUChars(conv,NULL,0,bytebuf,numbytes,&err);
+	if(err!=U_BUFFER_OVERFLOW_ERROR) { ucnv_close(conv); return nil; }
+
+	err=U_ZERO_ERROR;
+	unichar *charbuf=malloc(numchars*sizeof(unichar));
+	ucnv_toUChars(conv,charbuf,numchars,bytebuf,numbytes,&err);
+
+	ucnv_close(conv);
+
+	if(U_FAILURE(err))
+	{
+		free(charbuf);
+		return nil;
+	}
+	else
+	{
+		return [[[NSString alloc] initWithCharactersNoCopy:charbuf length:numchars freeWhenDone:YES] autorelease];
+	}
+}
+
++(NSData *)dataForString:(NSString *)string encodingName:(NSString *)encoding
+{
+	if([string length]==0) return [NSData data];
+
+	UErrorCode err;
+	UConverter *conv=ucnv_open(,&err);
+	if(!conv) return nil;
+
+	ucnv_setFromUCallBack(conv,UCNV_FROM_U_CALLBACK_STOP,NULL,NULL,NULL,&err);
+	if(U_FAILURE(err)) { ucnv_close(conv); return nil; }
+
+	int numchars=[string length];
+	unichar charbuf[numchars];
+	[string getCharacters:chars range:NSMakeRange(0,numchars)];
+
+	int numbytes=ucnv_fromUChars(conv,NULL,0,charbuf,numchars,&err);
+	if(err!=U_BUFFER_OVERFLOW_ERROR) { ucnv_close(conv); return nil; }
+
+	err=U_ZERO_ERROR;
+	char *bytebuf=malloc(numbytes);
+	ucnv_toUChars(conv,bytebuf,numbytes,charbuf,numchars,&err);
+
+	ucnv_close(conv);
+
+	if(U_FAILURE(err))
+	{
+		free(bytebuf);
+		return nil;
+	}
+	else
+	{
+		return [NSData dataWithBytesNoCopy:bytebuf length:numbytes freeWhenDone:YES];
+	}
+
+}
+
++(NSArray *)availableEncodingNames
+{
+}
+
+@end

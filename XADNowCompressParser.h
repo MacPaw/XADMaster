b857e3b48ae5f1466ef9ca9578f798064b98c11b
Dag Ågren <paracelsus@gmail.com>
2010-10-02 02:40:39 +0300

    Better Now Compress parser

--- XADNowCompressParser.h
+++ XADNowCompressParser.h
@@ -2,12 +2,16 @@
 
 @interface XADNowCompressParser:XADArchiveParser
 {
+	int totalentries,currentries;
+	NSMutableArray *entries;
 }
 
 +(int)requiredHeaderSize;
 +(BOOL)recognizeFileWithHandle:(CSHandle *)handle firstBytes:(NSData *)data name:(NSString *)name;
 
 -(void)parse;
+-(void)parseDirectoryWithParent:(XADPath *)parent numberOfEntries:(int)numentries;
+
 -(CSHandle *)handleForEntryWithDictionary:(NSDictionary *)dict wantChecksum:(BOOL)checksum;
 -(NSString *)formatName;
 
--- XADNowCompressParser.m
+++ XADNowCompressParser.m
@@ -15,11 +15,22 @@
 	int length=[data length];
 	const uint8_t *bytes=[data bytes];
 
-	if(length<24) return NO;
+	if(length<134) return NO;
 
-	// This is garbage! TODO: Figure out better detection
-	if(bytes[0]!=0x00||bytes[1]!=0x02||(bytes[2]!=0x00&&bytes[2]!=0x01)||bytes[3]!=0x60) return NO;
-	if(bytes[4]!=0x00||bytes[5]!=0x01||bytes[6]!=0x00||bytes[7]!=0x00) return NO;
+	if(bytes[0]!=0x00||bytes[1]!=0x02) return NO; // Check magic bytes.
+
+	if(CSUInt32BE(&bytes[8])>0xffff) return NO; // Check number of files. Assume no
+	if(CSUInt32BE(&bytes[8])==0) return NO; //  archive has more than 65535 files.
+
+	if(bytes[24]>31) return NO; // Check name length.
+	if(bytes[24]==0) return NO;
+
+	for(int i=0;i<bytes[24];i++)
+	if(bytes[25+i]<32) return NO; // Check for valid filename.
+
+	uint32_t sum=0;
+	for(int i=24;i<130;i++) sum+=bytes[i];
+	if(sum!=CSUInt32BE(&bytes[130])) return NO; // Check checksum.
 
 	return YES;
 }
@@ -29,28 +40,44 @@
 	[self setIsMacArchive:YES];
 
 	CSHandle *fh=[self handle];
-	NSMutableArray *entries=[NSMutableArray array];
 
 	[fh skipBytes:8];
-	int numfiles=[fh readUInt32BE];
+	totalentries=[fh readUInt32BE];
 	[fh skipBytes:12];
 
-	for(int i=0;i<numfiles;i++)
+	currentries=0;
+	entries=[NSMutableArray array];
+
+	[self parseDirectoryWithParent:[self XADPath] numberOfEntries:INT_MAX];
+
+	int numdicts=[entries count];
+	for(int i=0;i<numdicts;i++) [self addEntryWithDictionary:[entries objectAtIndex:i]];
+}
+
+-(void)parseDirectoryWithParent:(XADPath *)parent numberOfEntries:(int)numentries
+{
+	CSHandle *fh=[self handle];
+
+	for(int i=0;i<numentries && currentries<totalentries;i++,currentries++)
 	{
 		int namelen=[fh readUInt8];
 		NSData *namedata=[fh readDataOfLength:namelen];
-		[fh skipBytes:32-namelen];
-		[fh skipBytes:5];
+		[fh skipBytes:31-namelen];
+		[fh skipBytes:4];
 
-		uint32_t type=[fh readUInt32BE];
-		uint32_t creator=[fh readUInt32BE];
+		int flags=[fh readUInt16BE];
 
-		[fh skipBytes:8];
+		NSData *finderinfo=[fh readDataOfLength:16];
 
 		uint32_t creation=[fh readUInt32BE];
 		uint32_t modification=[fh readUInt32BE];
+		uint32_t access=[fh readUInt32BE];
 		
-		[fh skipBytes:24];
+		[fh skipBytes:16];
+
+		int numdirentries=[fh readUInt16BE];
+
+		[fh skipBytes:2];
 
 		uint32_t datasize=[fh readUInt32BE];
 		uint32_t rsrcsize=[fh readUInt32BE];
@@ -60,72 +87,83 @@
 		uint32_t datastart=[fh readUInt32BE];
 		uint32_t dataend=[fh readUInt32BE];
 
-		[fh skipBytes:4];
+		[fh skipBytes:4]; // Skip checksum.
 
-		XADPath *path=[self XADPathWithData:namedata separators:XADNoPathSeparator];
+		XADString *name=[self XADStringWithData:namedata];
+		XADPath *path=[parent pathByAppendingPathComponent:name];
 
-		NSDictionary *soliddict=[NSDictionary dictionaryWithObjectsAndKeys:
-			[NSNumber numberWithUnsignedInt:datastart],@"Offset",
-			[NSNumber numberWithUnsignedInt:dataend-datastart],@"Length",
+		NSMutableDictionary *shareddict=[NSMutableDictionary dictionaryWithObjectsAndKeys:
+			path,XADFileNameKey,
+			[NSDate XADDateWithTimeIntervalSince1904:modification],XADLastModificationDateKey,
+			[NSDate XADDateWithTimeIntervalSince1904:creation],XADCreationDateKey,
+			finderinfo,XADFinderInfoKey,
+			[NSNumber numberWithInt:flags],@"NowFlags",
 		nil];
 
-		if(datasize||!rsrcsize)
+		if(access) [shareddict setObject:[NSDate XADDateWithTimeIntervalSince1904:access] forKey:XADLastAccessDateKey];
+
+		if(flags&0x10) // Directory
 		{
 			NSMutableDictionary *dict=[NSMutableDictionary dictionaryWithObjectsAndKeys:
-				path,XADFileNameKey,
-				[NSNumber numberWithUnsignedInt:datasize],XADFileSizeKey,
-				[NSDate XADDateWithTimeIntervalSince1904:modification],XADLastModificationDateKey,
-				[NSDate XADDateWithTimeIntervalSince1904:creation],XADCreationDateKey,
-				[NSNumber numberWithUnsignedInt:type],XADFileTypeKey,
-				[NSNumber numberWithUnsignedInt:creator],XADFileCreatorKey,
-				//[NSNumber numberWithInt:finderflags],XADFinderFlagsKey,
-				//[self XADStringWithString:[self nameForMethod:0]],XADCompressionNameKey,
-
-				soliddict,XADSolidObjectKey,
-				[NSNumber numberWithLongLong:0],XADSolidOffsetKey,
-//				[NSNumber numberWithUnsignedInt:datasize],XADSolidLengthKey,
-				[NSNumber numberWithUnsignedInt:256],XADSolidLengthKey,
+				[NSNumber numberWithBool:YES],XADIsDirectoryKey,
 			nil];
 
-			if(datasize+rsrcsize) [dict setObject:[NSNumber numberWithUnsignedInt:(dataend-datastart)*datasize/(datasize+rsrcsize)] forKey:XADCompressedSizeKey];
-			else [dict setObject:[NSNumber numberWithUnsignedInt:0] forKey:XADCompressedSizeKey];
-
+			[dict addEntriesFromDictionary:shareddict];
 
 			[entries addObject:dict];
+
+			[self parseDirectoryWithParent:path numberOfEntries:numdirentries];
 		}
-
-		if(rsrcsize)
+		else
 		{
-			NSMutableDictionary *dict=[NSMutableDictionary dictionaryWithObjectsAndKeys:
-				path,XADFileNameKey,
-				[NSNumber numberWithBool:YES],XADIsResourceForkKey,
-				[NSNumber numberWithUnsignedInt:rsrcsize],XADFileSizeKey,
-				[NSNumber numberWithUnsignedInt:(dataend-datastart)*rsrcsize/(datasize+rsrcsize)],XADCompressedSizeKey,
-				[NSDate XADDateWithTimeIntervalSince1904:modification],XADLastModificationDateKey,
-				[NSDate XADDateWithTimeIntervalSince1904:creation],XADCreationDateKey,
-				[NSNumber numberWithUnsignedInt:type],XADFileTypeKey,
-				[NSNumber numberWithUnsignedInt:creator],XADFileCreatorKey,
-				//[NSNumber numberWithInt:finderflags],XADFinderFlagsKey,
-				//[self XADStringWithString:[self nameForMethod:0]],XADCompressionNameKey,
-
-				soliddict,XADSolidObjectKey,
-//				[NSNumber numberWithLongLong:datasize],XADSolidOffsetKey,
-//				[NSNumber numberWithUnsignedInt:rsrcsize],XADSolidLengthKey,
-				[NSNumber numberWithLongLong:0],XADSolidOffsetKey,
-				[NSNumber numberWithUnsignedInt:256],XADSolidLengthKey,
+			NSDictionary *soliddict=[NSDictionary dictionaryWithObjectsAndKeys:
+				[NSNumber numberWithUnsignedInt:datastart],@"Offset",
+				[NSNumber numberWithUnsignedInt:dataend-datastart],@"Length",
 			nil];
 
-			[entries addObject:dict];
+			if(datasize||!rsrcsize)
+			{
+				NSMutableDictionary *dict=[NSMutableDictionary dictionaryWithObjectsAndKeys:
+					[NSNumber numberWithUnsignedInt:datasize],XADFileSizeKey,
+					//[self XADStringWithString:[self nameForMethod:0]],XADCompressionNameKey,
+					soliddict,XADSolidObjectKey,
+					[NSNumber numberWithLongLong:0],XADSolidOffsetKey,
+//					[NSNumber numberWithUnsignedInt:datasize],XADSolidLengthKey,
+					[NSNumber numberWithUnsignedInt:256],XADSolidLengthKey,
+				nil];
+
+				[dict addEntriesFromDictionary:shareddict];
+
+				if(datasize+rsrcsize) [dict setObject:[NSNumber numberWithUnsignedInt:(dataend-datastart)*datasize/(datasize+rsrcsize)] forKey:XADCompressedSizeKey];
+				else [dict setObject:[NSNumber numberWithUnsignedInt:0] forKey:XADCompressedSizeKey];
+
+				[entries addObject:dict];
+			}
+
+			if(rsrcsize)
+			{
+				NSMutableDictionary *dict=[NSMutableDictionary dictionaryWithObjectsAndKeys:
+					[NSNumber numberWithBool:YES],XADIsResourceForkKey,
+					[NSNumber numberWithUnsignedInt:rsrcsize],XADFileSizeKey,
+					[NSNumber numberWithUnsignedInt:(dataend-datastart)*rsrcsize/(datasize+rsrcsize)],XADCompressedSizeKey,
+					//[self XADStringWithString:[self nameForMethod:0]],XADCompressionNameKey,
+					soliddict,XADSolidObjectKey,
+					[NSNumber numberWithLongLong:0],XADSolidOffsetKey,
+					[NSNumber numberWithUnsignedInt:256],XADSolidLengthKey,
+				nil];
+
+				[dict addEntriesFromDictionary:shareddict];
+
+				[entries addObject:dict];
+			}
 		}
 	}
-
-	for(int i=0;i<numfiles;i++) [self addEntryWithDictionary:[entries objectAtIndex:i]];
 }
 
 -(CSHandle *)handleForEntryWithDictionary:(NSDictionary *)dict wantChecksum:(BOOL)checksum
 {
-	return nil;
-//	return [self subHandleFromSolidStreamForEntryWithDictionary:dict];
+//	return nil;
+	return [self subHandleFromSolidStreamForEntryWithDictionary:dict];
 }
 
 -(CSHandle *)handleForSolidStreamWithObject:(id)obj wantChecksum:(BOOL)checksum

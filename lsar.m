3ead41218925b37c730131f33f1cc4d26b06c08d
Dag Ågren <paracelsus@gmail.com>
2013-07-04 23:45:11 +0300

    Bump unar and lsar versions

--- lsar.m
+++ lsar.m
@@ -5,7 +5,7 @@
 #import "CSJSONPrinter.h"
 #import "CommandLineCommon.h"
 
-#define VERSION_STRING @"v1.6"
+#define VERSION_STRING @"v1.7"
 
 #define EntryDoesNotNeedTestingResult 0
 #define EntryIsNotSupportedResult 1
--- unar.m
+++ unar.m
@@ -3,7 +3,7 @@
 #import "CSCommandLineParser.h"
 #import "CommandLineCommon.h"
 
-#define VERSION_STRING @"v1.6"
+#define VERSION_STRING @"v1.7"
 
 BOOL quietmode;
 

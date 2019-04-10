1f5a299a9c15bd6be99424648621220e6293c97d
Paul Taykalo <tt.kilew@gmail.com>
2019-04-10 11:52:14 +0300

    Add signature offset to most conflicting parsers

--- XADZipSFXParsers.h
+++ XADZipSFXParsers.h
@@ -25,7 +25,7 @@
 }
 
 +(int)requiredHeaderSize;
-+(BOOL)recognizeFileWithHandle:(CSHandle *)handle firstBytes:(NSData *)data name:(NSString *)name;
++(BOOL)recognizeFileWithHandle:(CSHandle *)handle firstBytes:(NSData *)data name:(NSString *)name propertiesToAdd:(NSMutableDictionary *)props;
 -(NSString *)formatName;
 
 @end

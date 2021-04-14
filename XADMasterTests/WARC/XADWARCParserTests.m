8c7a5ab2f3071711fa04ac2018bd8a49a33d52d8
Paul Taykalo <ptaykalo@macpaw.com>
2021-04-14 11:56:08 +0300

    Add a few more tests for WARC 1.1

--- XADMasterTests/WARC/XADWARCParserTests.m
+++ XADMasterTests/WARC/XADWARCParserTests.m
@@ -10,8 +10,11 @@
 #import "../../CSMemoryHandle.h"
 #import "../../XADWARCParser.h"
 
-@interface XADWARCParserTests : XCTestCase
+@interface XADWARCParserTestDelegate: NSObject
+@property(nonatomic, strong) NSDictionary * entry;
+@end
 
+@interface XADWARCParserTests : XCTestCase
 @end
 
 @implementation XADWARCParserTests
@@ -21,7 +24,7 @@
     [@"WARC/1.0\r\n"
      dataUsingEncoding:NSUTF8StringEncoding];
     XADMemoryHandle *handle = [CSMemoryHandle memoryHandleForReadingData:data];
-
+    
     XCTAssertTrue([XADWARCParser recognizeFileWithHandle:handle firstBytes:data name:@""]);
 }
 
@@ -30,8 +33,50 @@
     [@"WARC/1.1\r\n"
      dataUsingEncoding:NSUTF8StringEncoding];
     XADMemoryHandle *handle = [CSMemoryHandle memoryHandleForReadingData:data];
-
+    
     XCTAssertTrue([XADWARCParser recognizeFileWithHandle:handle firstBytes:data name:@""]);
+}
+
+- (void)testShouldCorrectlyFindsFileEntryIn10version {
+    NSURL * _Nullable fixtureURL = [[NSBundle bundleForClass:[XADWARCParserTests class]] URLForResource:@"warc-example-1.0" withExtension:nil subdirectory:@"WARCFixtures"];
+    NSData * data = [NSData dataWithContentsOfURL:fixtureURL];
+    XADMemoryHandle *handle = [CSMemoryHandle memoryHandleForReadingData:data];
+    
+    XCTAssertTrue([XADWARCParser recognizeFileWithHandle:handle firstBytes:data name:@""]);
+    
+    XADWARCParserTestDelegate * delegate = [XADWARCParserTestDelegate new];
+    XADWARCParser * parser = (XADWARCParser *)[XADWARCParser archiveParserForHandle:handle name:@""];
+    parser.delegate = delegate;
+    [parser parse];
+    
+    XCTAssertNotNil(delegate.entry);
+    XCTAssertEqualObjects(delegate.entry[@"XADFileSize"], @2396);
+}
+
+- (void)testShouldCorrectlyFindsEntryIn11Version {
+    NSURL * _Nullable fixtureURL = [[NSBundle bundleForClass:[XADWARCParserTests class]] URLForResource:@"warc-example-1.1" withExtension:nil subdirectory:@"WARCFixtures"];
+    NSData * data = [NSData dataWithContentsOfURL:fixtureURL];
+    XADMemoryHandle *handle = [CSMemoryHandle memoryHandleForReadingData:data];
+    
+    XCTAssertTrue([XADWARCParser recognizeFileWithHandle:handle firstBytes:data name:@""]);
+    
+    XADWARCParserTestDelegate * delegate = [XADWARCParserTestDelegate new];
+    XADWARCParser * parser = (XADWARCParser *)[XADWARCParser archiveParserForHandle:handle name:@""];
+    parser.delegate = delegate;
+    [parser parse];
+    
+    XCTAssertNotNil(delegate.entry);
+    XCTAssertEqualObjects(delegate.entry[@"XADFileSize"], @2396);
+}
+
+@end
+
+
+
+@implementation XADWARCParserTestDelegate
+
+- (void)archiveParser:(XADArchiveParser *)parser foundEntryWithDictionary:(NSDictionary *)dict {
+    self.entry = dict;
 }
 
 @end
